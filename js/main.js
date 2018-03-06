483231c977321b182fc580f2488a534243e710e3
Eausa <gregoire.pouthier@hotmail.fr>
2018-03-06 17:07:22 +0100

    u

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 const { app, BrowserWindow } = require('electron');
+const electron = require('electron');
 const path = require('path');
 const url = require('url');
 let win;
   
 function createWindow() {
-    win = new BrowserWindow();
+    const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize
+    win = new BrowserWindow({ width, height , titleBarStyle: 'hidden'});
 
     win.loadURL(url.format({
         pathname: path.join(__dirname, '../index.html'),
--- js/pathEditor.js
+++ js/pathEditor.js
@@ -1,4 +1,8 @@
 function path(fileName, content) {
     const container = document.getElementsByTagName('body')[0].appendChild(document.createElement('div'));
-    container.setAttribute('style', 'position:relative;top:60%;height:100%;')
+    container.setAttribute('class', 'containerPath');
+    const patheditor = container.appendChild(document.createElement('div'));
+    patheditor.setAttribute('id', 'patheditor');
+    const buttons = container.appendChild(document.createElement('div'));
+    buttons.setAttribute('class', 'buttons');
 }
